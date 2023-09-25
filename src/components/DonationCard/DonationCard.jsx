6186f4aa85d286b60738b0d0a7308e1da51a0aaa
tazbir01 <tajberahamad@gmail.com>
2023-09-25 17:29:24 +0600

    added card section and customize each card

---
+++ src/components/DonationCard/DonationCard.jsx
@@ -0,0 +1,21 @@
+
+
+const DonationCard = ({ donation }) => {
+    const { img1, title, category, text_color, card_background_color, background_color } = donation
+    console.log(donation)
+    return (
+        <div>
+            <div className="rounded-lg" style={{ backgroundColor: card_background_color }}>
+                <img src={img1} alt="" />
+                <div className="p-4">
+                    <div className="rounded-md inline-block px-2 py-1 my-2" style={{ backgroundColor: background_color }}>
+                        <p className="font-medium" style={{ color: text_color }}>{category}</p>
+                    </div>
+                    <h2 className="text-xl font-semibold" style={{ color: text_color }}>{title}</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DonationCard;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,37 @@
+import { useEffect, useState } from "react";
+import DonationCard from "../DonationCard/DonationCard";
 
 
 const Home = () => {
+    const [donations, setDonations] = useState([])
+
+    useEffect(() => {
+        fetch("donation.json")
+            .then(res => res.json())
+            .then(data => setDonations(data))
+    }, [])
+
     return (
-        <div className="h-[50vh] flex items-center justify-center" >
-            {/* <div className="max-w-6xl mx-auto" style={{backgroundImage: 'url(https://i.ibb.co/jRy3XVT/back-ground.jpg)'}}>
-                <h1 className="text-center text-5xl font-bold"></h1>
-                <div className="flex justify-center mt-10">
-                <input className="border w-80 px-4 py-2 rounded-l-lg" placeholder="Search Here..." type="text" />
-                <button className="bg-red-500 text-white px-5 rounded-r-lg">Search</button>
-                </div>
-            </div> */}
 
-
-            <div className="hero min-h-screen z-10" style={{ backgroundImage: 'url(https://i.ibb.co/jRy3XVT/back-ground.jpg)' }}>
-                <div className="hero-overlay bg-white bg-opacity-90"></div>
-                <div className="hero-content text-center text-neutral-content ">
-                    <div className="">
-                        <h1 className="mb-5 text-5xl text-black font-bold z-20">I Grow By Helping People In Need</h1>
-                        <div className="flex justify-center mt-10">
-                            <input className="border w-80 px-4 py-2 rounded-l-lg" placeholder="Search Here..." type="text" />
-                            <button className="bg-red-500 text-white px-5 rounded-r-lg">Search</button>
+        <div>
+            <div className="h-[50vh] flex items-center justify-center" >
+                <div className="hero min-h-screen z-10" style={{ backgroundImage: 'url(https://i.ibb.co/jRy3XVT/back-ground.jpg)' }}>
+                    <div className="hero-overlay bg-white bg-opacity-90"></div>
+                    <div className="hero-content text-center text-neutral-content ">
+                        <div className="">
+                            <h1 className="mb-5 text-5xl text-black font-bold z-20">I Grow By Helping People In Need</h1>
+                            <div className="flex justify-center mt-10">
+                                <input className="border w-80 px-4 py-2 rounded-l-lg" placeholder="Search Here..." type="text" />
+                                <button className="bg-red-500 text-white px-5 rounded-r-lg">Search</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="max-w-6xl mx-auto grid md:grid-cols-4 gap-6 mt-56 mb-24 ">
+                {
+                    donations.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+                }
             </div>
         </div>
     );
