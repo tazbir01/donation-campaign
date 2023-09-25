d91688b56bf9e8f816681c5ea5599f105514d810
tazbir01 <tajberahamad@gmail.com>
2023-09-25 23:18:23 +0600

    added donate button with overlay

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -13,15 +13,18 @@ const Details = () => {
     return (
         <div className="max-w-6xl mx-auto my-20 ">
             <div className="">
-                <div className="" >
-                <img className="" src={donation.img2} alt="" />
+                <div className="relative" >
+                    <img className="" src={donation.img2} alt="" />
+                    <div className="bg-black opacity-60 p-10  absolute bottom-0 w-full">
+                        <button className="text-white rounded-sm px-3 py-2" style={{ backgroundColor: donation.text_color }}>Donate ${donation.price}</button>
+                    </div>
                 </div>
-                <div className="opacity-90 ">
-                    <button className="text-white rounded-sm px-3 py-2" style={{backgroundColor:donation.text_color}}>Donate ${donation.price}</button>
-                </div>
+
             </div>
-            <h2 className="text-4xl font-bold mt-14 mb-6">{donation.title}</h2>
-            <p>{donation.description}</p>
+            <div>
+                <h2 className="text-4xl font-bold mt-14 mb-6">{donation.title}</h2>
+                <p>{donation.description}</p>
+            </div>
         </div>
     );
 };
