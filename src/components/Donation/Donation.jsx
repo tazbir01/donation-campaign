08b2811e04dfe15b20b724061ca192e622ad2a82
tazbir01 <tajberahamad@gmail.com>
2023-09-26 21:23:36 +0600

    added see all button for donation route

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -8,11 +8,12 @@ import Donated from "../Donated/Donated";
 const Donation = () => {
     const donations = useLoaderData()
     const [donation, setDonation] = useState([])
+    const [donationsLength, setDonationsLength] = useState(4)
 
     useEffect(() => {
         const storedDonationIds = getDonationData()
-        
-        if(donations.length > 0){
+
+        if (donations.length > 0) {
             const donated = donations.filter(donation => storedDonationIds.includes(donation.id))
             setDonation(donated)
             console.log(donated)
@@ -21,10 +22,16 @@ const Donation = () => {
 
 
     return (
-        <div className="grid md:grid-cols-2 max-w-6xl mx-auto gap-6 mt-20">
-            {
-                donation.map(item => <Donated key={item.id} donate={item}></Donated>)
-            }
+        <div className=" max-w-6xl mx-auto my-20">
+
+            <div className="grid md:grid-cols-2 gap-6">
+                {
+                    donation.slice(0,donationsLength).map(item => <Donated key={item.id} donate={item}></Donated>)
+                }
+            </div>
+            <div className={donationsLength === donation.length ? "hidden" : "flex justify-center mt-10"}>
+                <button onClick={()=>setDonationsLength(donation.length)} className="text-white font-medium bg-green-700 rounded-lg px-5 py-2">See All</button>
+            </div>
         </div>
     );
 };
